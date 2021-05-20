ec257c22fb341a7d89f2b1836275b0b3b01d5751
mamibetina <none>
2021-05-20 19:23:15 +0200

    First commit of the project.

---
+++ src/Controller/MainController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="main_home")
+     */
+    public function home(): Response
+    {
+        return $this->render('main/home.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/cgu", name="main_cgu")
+     */
+    public function cgu(): Response
+    {
+        return $this->render('main/cgu.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/aboutUs", name="main_aboutUs")
+     */
+    public function aboutUs(): Response
+    {
+        return $this->render('main/aboutUs.html.twig', [
+
+        ]);
+    }
+}
