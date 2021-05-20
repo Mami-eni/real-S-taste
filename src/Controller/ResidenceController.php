466a3cb4f571fa6074ff656a2774dfa3a1e97b74
mamibetina <none>
2021-05-20 20:55:45 +0200

    New twig files have been added, they are linked to the controller Residence. The nav document has been update to take account of this adding.

--- src/Controller/ResidenceController.php
+++ src/Controller/ResidenceController.php
@@ -9,12 +9,43 @@ use Symfony\Component\Routing\Annotation\Route;
 class ResidenceController extends AbstractController
 {
     /**
-     * @Route("/residence", name="residence")
+     * @Route("/list", name="residence_list")
      */
-    public function index(): Response
+    public function list(): Response
     {
-        return $this->render('residence/index.html.twig', [
-            'controller_name' => 'ResidenceController',
+        return $this->render('residence/list.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/list/detail", name="residence_detail")
+     */
+    public function detail(): Response
+    {
+        return $this->render('residence/detail.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/list/detail/update", name="residence_update")
+     */
+    public function update(): Response
+    {
+        return $this->render('residence/create.html.twig', [
+
+        ]);
+    }
+    /**
+     * @Route("/create", name="residence_create")
+     */
+    public function create(): Response
+    {
+        // si ça se passe bien redirection vers accueil ou detail
+        //sinon retour vers page create
+        return $this->render('residence/create.html.twig', [
+
         ]);
     }
 }
